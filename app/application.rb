f44593ff71a9723ea36018f5958054545757298c
Ranika Williams <rnkprice07@gmail.com>
2019-08-20 20:16:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,13 +9,13 @@ class Application
   
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
-      
-      else
-      resp.status = 400
-      resp.write  "Item not found"
-    end
+        if item = @@items.find{|i| i.name == item_name}
+        resp.write item.price
+        
+        else
+        resp.status = 400
+        resp.write  "Item not found"
+        end
     
     else
       resp.write "Route not found"

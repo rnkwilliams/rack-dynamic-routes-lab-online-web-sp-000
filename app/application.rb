bca3439ec13206bd67e21d2fc0bafef87d4b40ee
Ranika Williams <rnkprice07@gmail.com>
2019-08-20 20:07:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,21 +6,15 @@ class Application
    
    resp = Rack::Response.new
    req = Rack::Request.new(env)
-   
-    if req.path=="/songs"
-      resp.write "You requested the songs"
-    else
-      resp.write "Route not found"
-      resp.status = 404
-    end
-   
-   if req.path.match(/items/)
- 
+  
+    if req.path.match(/items/)
       song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
       song = @@songs.find{|s| s.title == song_title}
  
       resp.write song.artist
-   
+    else
+      resp.write "Route not found"
+      resp.status = 404
  end
   
 end

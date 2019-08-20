fdb5ae0623dd3498bc017440d317ed320f4145c8
Ranika Williams <rnkprice07@gmail.com>
2019-08-20 20:11:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,9 @@ class Application
       song = @@items.find{|i| i.name == item_name}
       resp.write item.price
     else
-      
+      resp.status = 400
+      resp.write  "Item not found"
+    end
       resp.write "Route not found"
       resp.status = 404
  end

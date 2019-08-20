d48527e5371a4e2889d774c5842785446bf0cd52
Ranika Williams <rnkprice07@gmail.com>
2019-08-20 20:06:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,12 @@ class Application
       resp.status = 404
     end
    
-   
+   if req.path.match(/items/)
+ 
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
    
  end
   
